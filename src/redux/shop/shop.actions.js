edc879c2797f535149a4bd73593bf5198fe37aa3
VirtueAndTerror <50920185+VirtueAndTerror@users.noreply.github.com>
2021-03-02 19:59:00 -0500

    added CollectionsOverview and Collectionspage containers, extrated fetching logic from Shop component, made use of thunk

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,7 +1,40 @@
-import { Shop } from 'react-bootstrap-icons';
 import ShopActionTypes from './shop.types';
 
-export const updateCollections = collectionsMap => ({
-	type: ShopActionTypes.UPDATE_COLLECTIONS,
+import {
+	firestore,
+	convertCollectionsSnapshotToMap,
+} from '../../firebase/firebase.utils';
+
+// Async actions are posibilitated by Redux Thunk
+
+export const fetchCollectionsStart = () => ({
+	type: ShopActionTypes.FETCH_COLLECTIONS_START,
+});
+
+export const fetchCollectionsSuccess = collectionsMap => ({
+	type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
 	payload: collectionsMap,
 });
+
+export const fetchCollectionsFailure = errorMessage => ({
+	type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+	payload: errorMessage,
+});
+
+// Redux-Thunk is going to detect this function,
+// take the returned dispatch function and ...
+export const fetchCollectionsStartAsync = () => {
+	return dispatch => {
+		const collectionRef = firestore.collection('collections');
+		dispatch(fetchCollectionsStart());
+
+		// Observables Pattern
+		collectionRef.onSnapshot(
+			async snapshot => {
+				const collectionsMap = await convertCollectionsSnapshotToMap(snapshot);
+				dispatch(fetchCollectionsSuccess(collectionsMap));
+			},
+			error => dispatch(fetchCollectionsFailure(error.message))
+		);
+	};
+};
